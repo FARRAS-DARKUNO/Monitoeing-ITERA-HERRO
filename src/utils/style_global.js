f7a6120f2cfd0be566ba7c8813f80112efe85a1e
mayonice1424 <friyandrailham14@gmail.com>
2022-09-07 22:43:07 +0700

    Menambahkan  styling template

--- src/utils/style_global.js
+++ src/utils/style_global.js
@@ -3,9 +3,130 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
+    //font style
+    header1:{
+        fontSize: 24,
+        fontFamily: 'Montserrat Bold 700',
+    },
+    header2:{
+        fontSize: 16,
+        fontFamily : 'Montserrat Bold 700',
+    },
+    header3:{
+        fontSize: 13,
+        fontFamily : 'Montserrat SemiBold 600',
+    },
+    subtitle : {
+        fontSize: 15,
+        fontFamily: 'Poppins-Bold',
+    },
+    body1:{
+        fontSize: 14,
+        fontFamily: 'Poppins-Regular',
+    },
+    body2:{
+        fontSize: 12,
+        fontFamily: 'Poppins-Regular',
+    },
+    body3:{
+        fontSize: 10,
+        fontFamily: 'Poppins-Regular',
+    },
+    caption:{
+        fontSize: 10,
+        fontFamily: 'Poppins-SemiBold',
+    },
+    button:{
+        fontSize: 18,
+        fontFamily: 'Poppins-Bold',
+    },
+    
+    //Colorstyle
 
-    }
+    primer:{
+        color:'#91C637',
+    },
+    primaryVariant:{
+        color:'#89BA35',
+    },
+    secondary:{
+        color:'#3DB35F',
+    },
+    secondaryVariant:{
+        color:'#36A155',
+    },
+    background:{
+        color:'#F6F6F6',
+    },
+    surface:{
+        color:'#FFFFFF',
+    },
+    gray:{
+        color:'#CFCFCF',
+    },
+    error:{
+        color:'#B00020',
+    },
+    onPrimary:{
+        color:'#FFFFFF',
+    },
+    onSecondary:{
+        color:'#FFFFFF',
+    },
+    onBackground:{
+        color:'#000000',
+    },
+    onSurface:{
+        color:'#000000',
+    },
+    onError:{
+        color:'#FFFFFF',
+    },
+
+    //background color
+    
+    backgroundPrimer:{
+        backgroundColor:'#91C637',
+    },
+    backgroundPrimaryVariant:{
+        backgroundColor:'#89BA35',
+    },
+    backgroundSecondary:{
+        backgroundColor:'#3DB35F',
+    },
+    backgroundSecondaryVariant:{
+        backgroundColor:'#36A155',
+    },
+    backgroundBackground:{
+        backgroundColor:'#F6F6F6',
+    },
+    backgroundSurface:{
+        backgroundColor:'#FFFFFF',
+    },
+    backgroundGray:{
+        backgroundColor:'#CFCFCF',
+    },
+    backgroundError:{
+        backgroundColor:'#B00020',
+    },
+    backgroundOnPrimary:{
+        backgroundColor:'#FFFFFF',
+    },
+    backgroundOnSecondary:{
+        backgroundColor:'#FFFFFF',
+    },
+    backgroundOnBackground:{
+        backgroundColor:'#000000',
+    },
+    backgroundOnSurface:{
+        backgroundColor:'#000000',
+    },
+    backgroundOnError:{
+        backgroundColor:'#FFFFFF',
+    },
+
+
+
 });
 
 export default styles
