804d622babd7d5a6148c5d58a1c0ae3f5fc464a1
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-10 02:11:08 +0700

    detail

--- src/page/detail_monitoring/detail_monitoring_page.js
+++ src/page/detail_monitoring/detail_monitoring_page.js
@@ -3,15 +3,67 @@ import {
     StyleSheet,
     View,
     Text,
+    StatusBar,
+    SafeAreaView,
+    TouchableOpacity,
+    Image,
 } from 'react-native';
+import styles from './detail_monitoring_style';
+import stylesGlobal from '../../utils/style_global';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 
 const DetailMonitoringPage = ({ navigation }) => {
+
+    let image = 'https://png.pngtree.com/template/20190316/ourmid/pngtree-water-logo-image_79174.jpg'
     return (
-        <View>
-            <Text style={styles.container}>
-                Hello wordA
-            </Text>
-        </View>
+        <SafeAreaView style={stylesGlobal.surface}>
+            <StatusBar
+                animated={true}
+                hidden={true}
+            />
+            <View style={[styles.header, stylesGlobal.backgroundPrimer]}>
+                <TouchableOpacity style={styles.buttonBack}>
+                    <Icon name="arrow-back" size={24} color="#ffff" />
+                    <View style={stylesGlobal.space10} />
+                    <Text style={[stylesGlobal.header2, { color: '#ffff' }]}>
+                        Green House 1
+                    </Text>
+                </TouchableOpacity>
+            </View>
+            <View style={stylesGlobal.enter20} />
+            <View style={styles.detail}>
+                <Text style={[stylesGlobal.header2, stylesGlobal.primer]}>
+                    Detail
+                </Text>
+                <Text style={[stylesGlobal.header3, stylesGlobal.primer]}>
+                    2021-06-06 17:38:52
+                </Text>
+            </View>
+            <View style={{ width: '100%', paddingHorizontal: 20 }}>
+                <View style={[styles.realTime, { backgroundColor: '#10B8DD' }]}>
+                    <View style={styles.dataRealtime}>
+                        <View style={styles.cisrcle}>
+                            <Image
+                                style={{ height: 24, width: 24 }}
+                                source={{ uri: image }}
+                            />
+                        </View>
+                        <View style={stylesGlobal.space10} />
+                        <View style={styles.textRealtime}>
+                            <Text style={[stylesGlobal.header3, { color: '#ffff' }]}>
+                                suhu riangan udara
+                            </Text>
+                            <Text style={[stylesGlobal.header2, { color: '#FFFF' }]}>{'75'}{'%'}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.checkOn}>
+                        <View style={[{ height: 10, width: 10, borderRadius: 100 }, stylesGlobal.backgroundSecondary]} />
+                        <Text style={[stylesGlobal.caption, stylesGlobal.primer]}> Online</Text>
+                    </View>
+                </View>
+            </View>
+
+        </SafeAreaView>
     );
 };
 
--- src/page/detail_monitoring/detail_monitoring_style.js
+++ src/page/detail_monitoring/detail_monitoring_style.js
@@ -3,9 +3,68 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-
-    }
+    header: {
+        width: '100%',
+        height: 90,
+        padding: 20,
+        flexDirection: "column-reverse",
+    },
+    buttonBack: {
+        // justifyContent: 'center',
+        alignContent: 'center',
+        flexDirection: 'row',
+        width: '60%'
+    },
+    detail: {
+        width: '100%',
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: 'red',
+        marginBottom: 10,
+    },
+    realTime: {
+        width: '100%',
+        height: 100,
+        borderRadius: 10,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+    },
+    dataRealtime: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '100%',
+        // backgroundColor: 'red',
+        width: '75%',
+    },
+    cisrcle: {
+        height: 45,
+        width: 45,
+        backgroundColor: '#ffff',
+        borderRadius: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textRealtime: {
+        height: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: '100%'
+    },
+    checkOn: {
+        width: '25%',
+        height: 25,
+        backgroundColor: '#ffff',
+        borderRadius: 10,
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
 });
 
 export default styles
