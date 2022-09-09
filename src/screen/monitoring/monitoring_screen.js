60e9a86dee6c125210e8b0c2eabd9e64ed0cb932
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-09 16:48:02 +0700

    else monitoring component

--- src/screen/monitoring/monitoring_screen.js
+++ src/screen/monitoring/monitoring_screen.js
@@ -11,6 +11,7 @@ import stylesGlobal from '../../utils/style_global';
 import ProgressCircle from 'react-native-progress-circle';
 import PersenMonitoring from '../../component/persen_monitoring';
 import DegreeMonitoring from '../../component/degree_monitoring';
+import ElseMonitoring from '../../component/else_monitoring';
 
 const MonitoringScreen = ({ navigation }) => {
     let DummieData = [
@@ -33,6 +34,16 @@ const MonitoringScreen = ({ navigation }) => {
             jenis: 'derajat',
             unit: 'Celcius',
             value: 29.8,
+        },
+        {
+            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTFdJtYeN28Zsl_3Vc9PejqIWbhdKASLgC95ZuILxqHVhzP-F7XvmfHc34zIHRkifWDJU&usqp=CAU',
+            color: '#B7A925',
+            name: 'Insensitas Cahaya',
+            status: 1,
+            tanggal: '2021-06-03 17:31:51',
+            jenis: 'else',
+            unit: 'Lux',
+            value: 22139.8,
         },
     ]
 
@@ -59,7 +70,16 @@ const MonitoringScreen = ({ navigation }) => {
                 value: DummieData[1].value,
                 unit: DummieData[1].unit,
             }} />
-
+            <ElseMonitoring data={{
+                icon: DummieData[2].icon,
+                color: DummieData[2].color,
+                name: DummieData[2].name,
+                status: DummieData[2].status,
+                tanggal: DummieData[2].tanggal,
+                jenis: DummieData[2].jenis,
+                value: DummieData[2].value,
+                unit: DummieData[2].unit,
+            }} />
 
 
         </View>
