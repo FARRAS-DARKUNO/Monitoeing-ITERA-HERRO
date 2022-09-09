2cbace30547dfd80909b999def69e5974caea09b
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-09 16:10:10 +0700

    degree monitoring componen

---
+++ src/component/degree_monitoring.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Image
+} from 'react-native';
+import stylesGlobal from '../utils/style_global';
+import ProgressCircle from 'react-native-progress-circle';
+
+const DegreeMonitoring = (props) => {
+
+    let data = props.data
+
+    return (
+        <TouchableOpacity style={styles.persenData}>
+            <View style={styles.insideCard}>
+                <View style={styles.titleAndIcon}>
+                    <Image source={{ uri: data.icon }} style={styles.imageIcon} />
+                    <View style={stylesGlobal.space10} />
+                    <Text style={[stylesGlobal.header3, { color: data.color }]}>
+                        {data.name}
+                    </Text>
+                </View>
+
+                <View style={{ alignItems: 'center' }}>
+                    <Text style={[stylesGlobal.header2, { color: data.color }]}>
+                        {data.value}{'°'}
+                    </Text>
+                    <Text style={[stylesGlobal.header2, { color: data.color }]}>
+                        {data.unit}
+                    </Text>
+                </View>
+
+                <View style={{ alignItems: 'center' }}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={[stylesGlobal.caption, stylesGlobal.primer]}>
+                            {'Status : '}
+                        </Text>
+                        {
+                            data.status == 1 ?
+                                <Text style={[stylesGlobal.caption, stylesGlobal.secondary]}>
+                                    online
+                                </Text> :
+                                <Text style={[stylesGlobal.caption, stylesGlobal.error]}>
+                                    offline
+                                </Text>
+                        }
+
+                    </View>
+                    <Text style={stylesGlobal.caption}>
+                        {data.tanggal}
+                    </Text>
+                </View>
+
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    insideCard: {
+        width: '100%',
+        alignItems: 'center',
+        height: '100%',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    persenData: {
+        width: '45%',
+        height: 180,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        borderColor: '#171717',
+        shadowOpacity: 1,
+        borderRadius: 10,
+        elevation: 1,
+        shadowRadius: 10,
+        padding: 20,
+        alignItems: 'center',
+    },
+    titleAndIcon: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageIcon: {
+        height: 24,
+        width: 24,
+        backgroundColor: 'red',
+        resizeMode: 'stretch',
+    },
+
+});
+
+export default DegreeMonitoring;
--- src/screen/monitoring/monitoring_screen.js
+++ src/screen/monitoring/monitoring_screen.js
@@ -10,6 +10,7 @@ import styles from './monitoring_style';
 import stylesGlobal from '../../utils/style_global';
 import ProgressCircle from 'react-native-progress-circle';
 import PersenMonitoring from '../../component/persen_monitoring';
+import DegreeMonitoring from '../../component/degree_monitoring';
 
 const MonitoringScreen = ({ navigation }) => {
     let DummieData = [
@@ -20,8 +21,19 @@ const MonitoringScreen = ({ navigation }) => {
             status: 1,
             tanggal: '2021-06-06 17:38:51',
             jenis: 'persen',
+            unit: 'persen',
             value: 50,
-        }
+        },
+        {
+            icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTFdJtYeN28Zsl_3Vc9PejqIWbhdKASLgC95ZuILxqHVhzP-F7XvmfHc34zIHRkifWDJU&usqp=CAU',
+            color: '#0B8559',
+            name: 'Suhu Lingkungan',
+            status: 0,
+            tanggal: '2021-06-06 17:31:51',
+            jenis: 'derajat',
+            unit: 'Celcius',
+            value: 29.8,
+        },
     ]
 
     return (
@@ -33,8 +45,23 @@ const MonitoringScreen = ({ navigation }) => {
                 status: DummieData[0].status,
                 tanggal: DummieData[0].tanggal,
                 jenis: DummieData[0].jenis,
-                value: DummieData[0].value
+                value: DummieData[0].value,
+                unit: DummieData[0].unit,
             }} />
+
+            <DegreeMonitoring data={{
+                icon: DummieData[1].icon,
+                color: DummieData[1].color,
+                name: DummieData[1].name,
+                status: DummieData[1].status,
+                tanggal: DummieData[1].tanggal,
+                jenis: DummieData[1].jenis,
+                value: DummieData[1].value,
+                unit: DummieData[1].unit,
+            }} />
+
+
+
         </View>
     );
 };
